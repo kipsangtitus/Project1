93ffd5e8fb6879689a33346130cd2797495e8ca5
kipsangtitus95@gmail.com <kipsangtitus95@gmail.com>
2019-10-29 19:35:11 +0300

    created a linq project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace New_folder__3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            {
+                
+            }
+        }
+    }
+}
